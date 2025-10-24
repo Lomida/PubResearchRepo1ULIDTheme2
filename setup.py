b29ec5b80269437b56423f090e8d9a8db08ca915
Lomida <61120829+Lomida@users.noreply.github.com>
2025-10-24 10:40:35 -0400

    5
    5

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='unary-theme',
+    version='0.1.0',
+    description='the unary lab theme',
+    author='Devon Lister',
+    author_email='DevonLister@yahoo.com',
+    url='https://github.com/Lomida/PubResearchRepo1ULIDTheme2',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'sphinx>=4.0',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: Documentation',
+        'Topic :: Software Development :: Documentation',
+    ],
+    entry_points={
+        'sphinx.html_themes': [
+            'my_custom_theme = my_custom_sphinx_theme',
+        ]
+    },
+)
---
+++ unary_theme/__init__.py
@@ -0,0 +1,30 @@
+"""unary lab theme."""
+from pathlib import Path
+from os import path
+
+__version__ = '0.1.0'
+
+
+# Depreciated in newer Sphinx
+def get_html_theme_path():
+    """Return list of HTML theme paths."""
+    return [path.abspath(path.dirname(__file__))]
+
+
+def get_path():
+    """Return path to theme directory.
+    
+    This is used by Sphinx's html_theme_path configuration.
+    """
+    return path.abspath(path.dirname(path.dirname(__file__)))
+
+
+def setup(app):
+    """Setup function called by Sphinx."""
+    app.add_html_theme('unary_theme', path.abspath(path.dirname(__file__)))
+    
+    return {
+        'version': __version__,
+        'parallel_read_safe': True,
+        'parallel_write_safe': True,
+    }
