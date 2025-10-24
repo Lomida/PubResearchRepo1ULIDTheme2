2552f980bf71167857506994cba5e8b8fea5573f
Lomida <61120829+Lomida@users.noreply.github.com>
2025-10-24 11:13:42 -0400

    10
    10

--- unary_theme/conf.py
+++ unary_theme/conf.py
@@ -8,11 +8,14 @@ html_theme_path = ['_themes']
 
 html_theme_options = {
     'logo_image': 'logo.svg',
-    'logo_link': 'https://example.com',
+    'logo_link': 'https://www.unarylab.com/,
+
     'nav_link_1_text': 'Documentation',
     'nav_link_1_url': 'https://docs.example.com',
+
     'nav_link_2_text': 'GitHub',
     'nav_link_2_url': 'https://github.com/yourusername/yourrepo',
+    
     'nav_link_3_text': 'About',
     'nav_link_3_url': 'https://example.com/about',
 }
